f21b3affa0a28163732f530246245cfcbad86910
Seraphine Marie <seraphine.marie@ieseg.fr>
2023-02-21 14:57:22 +0100

    jhbvvbh

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Read All
+  get 'tasks', to: 'tasks#index'
+
+  # Create
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # Read One
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # Update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
